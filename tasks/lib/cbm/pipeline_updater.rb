c55a95e367e3fa7ec055c092c1a11e5f0a4d4fbc
Igor Poljanskij <igor.poljanskij@volkswagen.de>
2018-01-08 14:09:03 +0100

    Added logging

--- tasks/lib/cbm/pipeline_updater.rb
+++ tasks/lib/cbm/pipeline_updater.rb
@@ -57,6 +57,7 @@ module Cbm
       log 'Trying to download fly executable...'
 
       fly_download_url = "#{url}/api/v1/cli?arch=amd64&platform=linux"
+      log fly_download_url
       read_binary_open_mode = 'rb'
       stream = open(
         fly_download_url,
