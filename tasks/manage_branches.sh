3eb561168a11711bed9ccf0a877ba613ef054ee5
Igor Poljanskij <igor.poljanskij@volkswagen.de>
2018-01-09 13:56:12 +0100

    check if we are able to reach concourse

--- tasks/manage_branches.sh
+++ tasks/manage_branches.sh
@@ -15,9 +15,9 @@ echo $HTTP_PROXY $HTTPS_PROXY
 
 echo $CONCOURSE_TEAM  $CONCOURSE_USERNAME $CONCOURSE_PASSWORD
 
-ping 10.2.29.246
 
-curl --insecure -sSL https://10.2.29.246/api/v1/info
+
+curl --insecure -SsL https://concourse-sdc.wob.vw.vwg/api/v1/info
 
 ./fly --target=concourse login -k --verbose --concourse-url="$CONCOURSE_URL" --team-name=kpm -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWORD
 
