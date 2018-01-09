0fc359794da7be444920c23d9fa08d5c3f726beb
Igor Poljanskij <igor.poljanskij@volkswagen.de>
2018-01-09 13:38:24 +0100

    using shellscript for fly login

--- tasks/manage_branches.sh
+++ tasks/manage_branches.sh
@@ -12,9 +12,8 @@ echo "PROXIES"
 
 echo $HTTP_PROXY $HTTPS_PROXY
 
-./fly help
 
 echo $CONCOURSE_TEAM  $CONCOURSE_USERNAME $CONCOURSE_PASSWORD
 
-./fly --target=concourse login -k --verbose --concourse-url=$CONCOURSE_URL --team-name=kpm -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWORD
+./fly --target=concourse login -k --verbose --concourse-url="$CONCOURSE_URL" --team-name=kpm -u "$CONCOURSE_USERNAME" -p "$CONCOURSE_PASSWORD"
 
