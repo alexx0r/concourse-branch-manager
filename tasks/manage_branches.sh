acb25c80e5edc7f1bf5ed66279dff273743da560
Igor Poljanskij <igor.poljanskij@volkswagen.de>
2018-01-09 10:58:07 +0100

    Logggin into concourse with fly

--- tasks/manage_branches.sh
+++ tasks/manage_branches.sh
@@ -4,9 +4,13 @@ echo "$CONCOURSE_URL"
 
 curl --verbose --insecure $CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux > fly
 
-ls
+ls -a
 
 chmod +x fly
 
-./fly --help
+./fly help
+
+echo $CONCOURSE_TEAM  $CONCOURSE_USERNAME $CONCOURSE_PASSWORD
+
+./fly --target=concourse login --verbose --concourse-url=$CONCOURSE_URL $CONCOURSE_TEAM -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWORD
 
