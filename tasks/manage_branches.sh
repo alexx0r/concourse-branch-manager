cb59dd6cc9573b13f8f4a74d9b3b472aaaffea0f
Igor Poljanskij <igor.poljanskij@volkswagen.de>
2018-01-09 11:00:29 +0100

    Edited dowloading of curl

--- tasks/manage_branches.sh
+++ tasks/manage_branches.sh
@@ -2,7 +2,7 @@
 
 echo "$CONCOURSE_URL"
 
-curl --verbose --insecure $CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux > fly
+curl --verbose --insecure $CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux
 
 ls -a
 
